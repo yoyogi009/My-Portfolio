06516929e0572c563fd8c6ae8f02cb00860b663d
Yogesh Maurya <yogeshykm9@gmail.com>
2020-08-31 01:27:57 +0530

    Updates

---
+++ precache-manifest.05cebd8839e8dcb842ff1d8cf20f02cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f4c72220080ed471cd4d21c07a013d",
+    "url": "/yoyogi009.github.io/My-Portfolio/index.html"
+  },
+  {
+    "revision": "c890d276a07e8644c969",
+    "url": "/yoyogi009.github.io/My-Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ece8a9c810106786f1f7",
+    "url": "/yoyogi009.github.io/My-Portfolio/static/css/main.14007d55.chunk.css"
+  },
+  {
+    "revision": "c890d276a07e8644c969",
+    "url": "/yoyogi009.github.io/My-Portfolio/static/js/2.187ed17f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yoyogi009.github.io/My-Portfolio/static/js/2.187ed17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece8a9c810106786f1f7",
+    "url": "/yoyogi009.github.io/My-Portfolio/static/js/main.938a8ede.chunk.js"
+  },
+  {
+    "revision": "7d7ea0b6c1be86be2f2a",
+    "url": "/yoyogi009.github.io/My-Portfolio/static/js/runtime-main.080c098c.js"
+  },
+  {
+    "revision": "2bf4f0d455fd0a594c28f36ddaf4e8dd",
+    "url": "/yoyogi009.github.io/My-Portfolio/static/media/profile-img.2bf4f0d4.jpg"
+  }
+]);
